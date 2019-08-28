a4fbebcc2db96604bd14e321f330762951606963
realvesd <43302742+realvesd@users.noreply.github.com>
2019-08-27 21:02:19 -0300

    Create 1.java

---
+++ 1.java
@@ -0,0 +1,19 @@
+package declarandovariaveis;
+
+public class Condicoes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int z = 23;
+		if (z == 10) {
+			System.out.println("z é igual a 10:") ;
+			} else {
+				System.out.println("z não é igual a 10:)" );
+			}
+				System.out.println("isso é enrolação");
+				
+		
+		
+	}
+
+}
